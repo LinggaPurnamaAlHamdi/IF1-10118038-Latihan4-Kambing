914b953998bcedbf9208d5455d0552168fff7e6e
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-09-25 12:32:31 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan4-Kambing/src/if1/pkg10118038/latihan4/kambing/IF110118038Latihan4Kambing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan4.kambing;
+
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : Program ini berisi program yang menampilkan
+ * Jumlah Kambing
+ */
+public class IF110118038Latihan4Kambing {
+
+    public void tambahKambing() {
+        //deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IF110118038Latihan4Kambing kambingJantan = new IF110118038Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
